38533c563ea1b5bb2d7f947bc92da2608089e44c
Abd <abd.arstiae@gmail.com>
2021-07-25 01:43:10 -0700

    Initial commit

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'NzgxMTY3NTc2NzkyNTYzNzUz.X75s_w.1bt17Dqe278h3C-Aqs2yKb8ZmHg',
+        prefix: '.m',
+        activity: '.m',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
